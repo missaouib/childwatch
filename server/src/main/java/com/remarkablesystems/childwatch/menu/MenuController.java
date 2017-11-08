e6d67c575dfc2fd7444ee6596a627b92c4ce7a32
Matt Young <matthew.young@bisonworks.com>
2017-11-08 14:30:17 -0700

    CW2 - BETA 21 prototype of printing menu

--- server/src/main/java/com/remarkablesystems/childwatch/menu/MenuController.java
+++ server/src/main/java/com/remarkablesystems/childwatch/menu/MenuController.java
@@ -1,10 +1,11 @@
 package com.remarkablesystems.childwatch.menu;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,32 +13,105 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.xhtmlrenderer.pdf.ITextRenderer;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
-import com.itextpdf.text.DocumentException;
+import com.openhtmltopdf.extend.FSUriResolver;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 
 @RestController
 public class MenuController {
 	
-	@RequestMapping( "/generatemenu" )
-	ResponseEntity<byte[]> generateMenu() {
+	public static class UriResolver implements FSUriResolver {
+		/**
+		 * Resolves the URI; if absolute, leaves as is, if relative, returns an
+		 * absolute URI based on the baseUrl for the agent.
+		 *
+		 * @param uri
+		 *            A URI, possibly relative.
+		 *
+		 * @return A URI as String, resolved, or null if there was an exception
+		 *         (for example if the URI is malformed).
+		 */
+		@Override
+		public String resolveURI(String baseUri, String uri) {
+			if (uri == null || uri.isEmpty())
+				return null;
+			
+			try {
+				URI possiblyRelative = new URI(uri);
+				
+				if (possiblyRelative.isAbsolute()) {
+					return possiblyRelative.toString();
+				} else {
+				    if(baseUri.startsWith("jar")) {
+				    	// Fix for OpenHTMLtoPDF issue-#125, URI class doesn't resolve jar: scheme urls and so returns only
+				    	// the relative part on calling base.resolve(relative) so we use the URL class instead which does
+				    	// understand jar: scheme urls.
+				    	URL base = new URL(baseUri);
+				        URL absolute = new URL(base, uri);
+				        return absolute.toString();
+				    } else {
+						URI base = new URI(baseUri);
+						URI absolute = base.resolve(uri);
+						return absolute.toString();				    	
+				    }
+				}
+			} catch (URISyntaxException e) {
+				System.err.println("When trying to load uri(" + uri + ") with base URI(" + baseUri + "), one or both were invalid URIs." );
+				return null;
+			} catch (MalformedURLException e) {
+				System.err.println("When trying to load uri(" + uri + ") with base jar scheme URI(" + baseUri + "), one or both were invalid URIs." );
+				return null;
+			}
+		}
+	}
+	
+	
+	String renderTemplate( String templateName ) {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML");
+		 
+		TemplateEngine templateEngine = new TemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+		 
+		Context context = new Context();
+		context.setVariable("name", "User");
+		 
+		// Get the plain HTML with the resolved ${name} variable!
+		return templateEngine.process(templateName, context);		
+	}
+	
+	void generatePdf( OutputStream outputStream ) {
+		PdfRendererBuilder builder = new PdfRendererBuilder();
+
+		String html = renderTemplate( "template" );
+
+		builder.withHtmlContent(html, MenuController.class.getResource("/template.html").toExternalForm() );
 		
+		builder.toStream(outputStream);
+		builder.useUriResolver( new UriResolver() );
 		
-		String html = "<h1>Hello world</h1>";
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		ITextRenderer renderer = new ITextRenderer();
-		renderer.setDocumentFromString(html);
-		renderer.layout();
 		try {
-			renderer.createPDF(outputStream);
-		} catch (DocumentException | IOException e) {
+			builder.run();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	@RequestMapping( "/generatemenu" )
+	ResponseEntity<byte[]> generateMenu() {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+		generatePdf( outputStream );
 		
 		HttpHeaders headers = new HttpHeaders();
 	    headers.setContentType(MediaType.parseMediaType("application/pdf"));
 	    String filename = "output.pdf";
-	    headers.setContentDispositionFormData(filename, filename);
+	    headers.add("content-disposition", "inline;filename=" + filename);
 	    headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
 	    ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(outputStream.toByteArray(), headers, HttpStatus.OK);
 	    return response;
