8a2d30c72259e15ccd7c78b661f88b69adba8a91
Matt Young <matthew.young@bisonworks.com>
2018-01-31 12:46:26 -0700

    CW2 - tenant/user changed with ageGroups/mealTypes/theme

--- server/src/main/java/com/remarkablesystems/childwatch/config/multitenant/TenantFetcher.java
+++ server/src/main/java/com/remarkablesystems/childwatch/config/multitenant/TenantFetcher.java
@@ -1,7 +1,6 @@
 package com.remarkablesystems.childwatch.config.multitenant;
 
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import org.slf4j.Logger;
@@ -49,6 +48,24 @@ public class TenantFetcher extends UnboundTenantTask<Tenant>{
         	tenantRepo.save(tenant);
         	logger.info("Creating tenant {}", tenant.getName() );
         }
+        else {
+			tenant.setSupportingAge0_5MO( !Arrays.asList(ageGroups).contains("AGE_0_5MO") );
+			tenant.setSupportingAge6_11MO( !Arrays.asList(ageGroups).contains("AGE_6_11MO") );
+			tenant.setSupportingAge1YR( !Arrays.asList(ageGroups).contains("AGE_1YR") );
+			tenant.setSupportingAge2YR( !Arrays.asList(ageGroups).contains("AGE_2YR") );
+			tenant.setSupportingAge3_5YR( !Arrays.asList(ageGroups).contains("AGE_3_5YR") );
+			tenant.setSupportingAge6_12YR( !Arrays.asList(ageGroups).contains("AGE_6_12YR") );
+			tenant.setSupportingAge13_18YR( !Arrays.asList(ageGroups).contains("AGE_13_18YR") );
+			tenant.setSupportingAgeAdult( !Arrays.asList(ageGroups).contains("AGE_ADULT") );
+			tenant.setSupportingBreakfast( !Arrays.asList(mealTypes).contains("B"));
+			tenant.setSupportingAMSnack( !Arrays.asList(mealTypes).contains("A"));
+			tenant.setSupportingLunch( !Arrays.asList(mealTypes).contains("L"));
+			tenant.setSupportingPMSnack( !Arrays.asList(mealTypes).contains("P"));
+			tenant.setSupportingSupper( !Arrays.asList(mealTypes).contains("S"));
+			tenant.setSupportingEVSnack( !Arrays.asList(mealTypes).contains("E"));
+			
+			tenant = tenantRepo.save(tenant);
+        }
         
         if( tenant != null ) {
 	        // if there isnt a user id - use the tenant id
@@ -66,6 +83,10 @@ public class TenantFetcher extends UnboundTenantTask<Tenant>{
 	        	if( this.adminUser ) user.addAuthority("ADMIN");
 	        	userRepo.save(user);
 	        	logger.info( "Creating user {} for {}", user.getUsername(), tenant.getName() );
+	        }
+	        else {
+	        	user.setTheme( theme != null && theme.length() > 0 ? theme : "readable" );
+	        	user = userRepo.save(user);
 	        }
         }
         
